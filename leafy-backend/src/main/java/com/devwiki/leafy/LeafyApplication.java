9daad53c4d9b4020fea46347469ab556dfe2d8db
Eguanna <jiwhan13@kumoh.ac.kr>
2024-06-19 14:40:56 +0000

    +) 아몰랑

--- leafy-backend/src/main/java/com/devwiki/leafy/LeafyApplication.java
+++ leafy-backend/src/main/java/com/devwiki/leafy/LeafyApplication.java
@@ -3,7 +3,9 @@ package com.devwiki.leafy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = "com.devwiki.leafy"
+)
 public class LeafyApplication {
 
 	public static void main(String[] args) {
